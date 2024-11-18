7b04d47aa2752bd9f0dc52f496cbc11a4fe81e4b
Erogova Elena <erogova.ed@phystech.edu>
2024-11-18 23:09:04 +0300

    add changes

--- Integrator/laba3.hpp
+++ Integrator/laba3.hpp
@@ -4,23 +4,42 @@
 #include <array>
 #include <cmath>
 
-template<typename RealType, unsigned int N>
-std::array<RealType, N> Transfer(std::array<RealType, N>& nodes, RealType start, RealType end) {
-    std::array<RealType, N> points;
 
-    for(int i = 0; i < N; i++){
-        points[i] = (start + end)/2 + (end - start) * nodes[i]/2;
+template<typename RealType, unsigned int N>
+RealType integrate(const RealType& start, const RealType& end){
+
+    std::array<double, N> nodes, weight;
+
+//    if(N == 3){
+//        nodes = { std::sqrt(3.0 / 5), 0, -std::sqrt(3.0 / 5) };
+//        weight = { 5.0 / 9, 8.0 / 9, 5.0 / 9 };
+//    }
+
+    if(N == 4){
+        nodes = {-0.8611363115940526, -0.3399810435848563, 0.3399810435848563, 0.8611363115940526};
+        weight = {0.3478548451374539, 0.6521451548625461, 0.6521451548625461, 0.3478548451374539};
     }
 
-    return points;
-}
 
-template<typename RealType, unsigned int N>
-RealType Integrator(std::array<RealType, N>& points, std::array<RealType, N>& weight, RealType start, RealType end){
     RealType result = 0;
 
     for(int j = 0; j < N; j++){
-        result += (end - start) * weight[j] * std::sin(points[j])/2;
+        result += weight[j] * std::sin((start + end)/2 + (end - start)/2 * nodes[j]);
+    }
+
+    return (end - start) / 2 * result;
+}
+
+template<typename RealType, unsigned int N>
+RealType Integrate(const RealType& start, const RealType& end, const RealType& dx){
+    RealType result = 0;
+
+    for(RealType x = start; x <= end; x += dx){
+        if (x + dx >= end) {
+            result += integrate<RealType, N>(x, end);
+            break;
+        }
+        result += integrate<RealType, N>(x, x + dx);
     }
 
     return result;
